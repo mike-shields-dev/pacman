de3f752f19aec0247960dca56e01149827488e26
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-03-31 20:41:10 +0100

    Refactor Boundary class > draw method to take context

--- Boundary.js
+++ Boundary.js
@@ -1,14 +1,13 @@
 class Boundary {
     constructor({ ctx, position }) {
-        this.ctx = ctx;
         this.position = position;
         this.width = 40;
         this.height = 40;
     }
 
-    draw() {
-        this.ctx.fillStyle = 'blue';
-        this.ctx.fillRect(
+    draw(ctx) {
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(
             this.position.x, 
             this.position.y, 
             this.width, 
