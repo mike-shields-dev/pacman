d2570e6ebb8c01f2108db4bd21c69234ed8d03da
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-03-31 20:41:31 +0100

    feat: Create boundaries array

---
+++ boundaries.js
@@ -0,0 +1,11 @@
+import Boundary from './Boundary.js';
+
+const boundaries = [
+    new Boundary({ position: { x: 40,  y: 40 } }),
+    new Boundary({ position: { x: 80,  y: 40 } }),
+    new Boundary({ position: { x: 120, y: 40 } }),
+    new Boundary({ position: { x: 160, y: 40 } }),
+    new Boundary({ position: { x: 200, y: 40 } }),
+];
+
+export default boundaries;
