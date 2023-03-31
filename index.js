6d3b365fe1d39759c004a748bd50ce3a7c08778a
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-03-31 20:59:23 +0100

    feat: Add 2d map array and iterate to draw boundaries

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import boundaries from "./boundaries.js";
+import Boundary from './Boundary.js';
+import map from './map.js';
 
 const canvas = document.querySelector('canvas');
 
@@ -6,7 +7,15 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-boundaries.forEach(b => b.draw(ctx));
+map.forEach((row, _y) => {
+    row.forEach((col, _x) => {
+        const position = { x: _x * 40, y: _y * 40}
+        const boundary = new Boundary({ position });
+        const isChecked = map[_y][_x] === 1;
+
+        if(isChecked) boundary.draw(ctx);
+    });
+});
 
 function loop() {
 
